aa4b9066ba5ba040551ffcf1ac138e0f295e4108
Nikita <nteplitskiy@TX150>
2021-01-28 14:33:42 -0500

    q1 commit

---
+++ hw1/q1.m
@@ -0,0 +1,48 @@
+%% EpicQUESTion 1 
+M = 1e6;
+%% Part A
+% P(Y=18) = P(X=3)^6 = (1/3)^6 = 0.0014
+% 3^6 permutations
+rolls = randi(3,6,M);
+net = sum(rolls,1);
+% What fraction of experiments result in 18?
+sum(net==18)/M
+
+%% Part B 
+% P(Z=18) = P(Y)*3 = 0.0042
+rolls = randi(3,6,M,3);
+x = sum(rolls,1);
+y = sum(x==18,3);
+% Fraction of experiments resulting in at least one 18
+sum(y>0)/M
+
+%% Part C
+% P(A) = P(Z=18)^6 = 5.4890e-15 \approx 0
+% Array would be too large
+M = 1e4;
+n = 0;
+for i = 0:M
+    rolls = randi(3,6,3,6);
+    x = sum(rolls,1);
+    y = sum(x==18,2);
+    n = n + double(sum(y>0)==6);
+end
+% Will almost always be zero
+n
+n/M
+%% Modified Part C
+% Skip initial die generation
+
+%% Part D
+% P(A) = P(Z=9)^6 = (3*P(Y=9)^6 = P(3*(25*(1/3)^6)) = 1.1858e-06
+clc
+M = 1e9;
+n = 0;
+parfor i = 0:M
+    rolls = randi(3,6,3,6);
+    x = sum(rolls,1);
+    y = sum(x==18,2);
+    n = n + double(sum(y>0)==6);
+end
+n
+n/M
