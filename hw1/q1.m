4a5160a6effc01be4c415abe27baf39158a5ea30
Nikita <nteplitskiy@TX150>
2021-01-29 13:52:11 -0500

    added comments and made prettier

--- hw1/q1.m
+++ hw1/q1.m
@@ -1,43 +1,47 @@
 %% EpicQUESTion 1 
-M = 5;
-M = 1e6;
+%M = 5;
+M = 1e6; % number of experiments to perform 
 %% Part A
-% P(Y=18) = P(X=6)^3 = (1/6)^3 = 0.0046
+% $P(Y=18) = P(X=6)^3 = (1/6)^3 = 0.0046$
+
 % 6^3 permutations
-rolls = randi(6,3,M);
-net = sum(rolls,1);
+rolls = randi(6,3,M); % generate set of 3d6
+net = sum(rolls,1); % sum rolls to get score
 % What fraction of experiments result in 18?
 sum(net==18)/M
 
 %% Part B 
-% P(Z=18) = P(Y)*3 = 0.0138
-rolls = randi(6,3,M,3);
-x = sum(rolls,1);
-y = sum(x==18,3);
-% Fraction of experiments resulting in at least one 18
+% $P(Z=18) = P(Y)*3 = 0.0138$
+rolls = randi(6,3,M,3); % generate 3 trials of 3d6
+x = sum(rolls,1); % combine rolls for score 
+y = sum(x==18,3); % calculate how many scores are 18
+% Fraction of experiments resulting in score of 18
 sum(y>0)/M
 
 %% Part C
-% P(A) = P(Z=18)^6 = 6.9068e-12
-% Array would be too large
+% $P(A) = P(Z=18)^6 = 6.9068e-12$
+% Too large to represent with array 
 M = 1e4;
 n = 0;
 for i = 0:M
-    rolls = randi(6,3,3,6);
-    x = sum(rolls,1);
-    y = sum(x==18,2);
-    n = n + double(sum(y>0)==6);
+    rolls = randi(6,3,3,6); % generate 6 trials of 3d6
+    x = sum(rolls,1); % get 3 scores 
+    y = sum(x==18,2); % check if 18
+    n = n + double(sum(y>0)==6); % increment the number of Fontaines
 end
 % Will almost always be zero
 n
+% Fraction of experiments 
 n/M
 
 %% Part D
+% This part is not correct 
 % 9 must be the greatest value in the best of 3 case
 clc
-M = 1e6;
+%M = 1e6;
+M = 1e4;
 n = 0;
-parfor i = 0:M
+for i = 0:M
     rolls = randi(6,3,3,6);
     x = sum(rolls,1);
     y = sum(x==18,2);
--- hw1/q2.m
+++ hw1/q2.m
@@ -1,29 +1,40 @@
 %% EpicQUESTion 2
 M = 10;
-M = 1e6;
+M = 1e6; % number of experiments to perform 
 %% Part A
-% Average troll health = .25*1 + .25*2 + .25*3 +.25*4 = 2.5
-hp = randi(4,M,1);
-hp_ave = mean(hp) % average troll health
-% Average firefball damage = 0*1 + .25*2 + .50*3 + .25*4 = 3
-f = randi(2,M,2);
-f_dmg = sum(f,2);
-f_ave = mean(f_dmg) % average fireball damage
-sum(f_dmg>3)/M % probability damage > 3
+% Average troll health $= .25*1 + .25*2 + .25*3 +.25*4 = 2.5$
+hp = randi(4,M,1); % set of 1d4
+hp_ave = mean(hp); % average troll health
+fprintf("Average troll health: %f", hp_ave);
+
+%%
+% Average firefball damage $= 0*1 + .25*2 + .50*3 + .25*4 = 3$
+f = randi(2,M,2); % set of 2d2
+f_dmg = sum(f,2); % compute damage 
+f_ave = mean(f_dmg); % average fireball damage
+f_3 = sum(f_dmg>3)/M; % probability damage > 3
+fprintf("Average fireball damage: %f\n", f_ave);
+fprintf("Probability fireball >3: %f", f_3);
+
 %% Part B
-% no
+% I don't know how to do this nice 
+
 %% Part C
 % P ~ 0.177
 hp = randi(4,M,6); % make M banks of 6 trolls
-f = randi(2,M,2,6); % throw 2 die for each troll
-f_dmg = squeeze(sum(f,2)); 
+f = randi(2,M,2,6); % throw 2 die for each spell
+f_dmg = squeeze(sum(f,2)); % compute fireball damage
 kia = (hp - f_dmg) > 0; % attack troll, see if dead
-sum(sum(kia,2) == 0)/M % count the dead
+dead = sum(sum(kia,2) == 0)/M; % count the dead
+fprintf("Probability of 6 kill: %f", dead);
 %% Part D
 % Each troll killed independently
-% Expected troll starting health .5*3 + .5*4 = 3.5
-% Expected fireball damage 1/3*2 + 2/3*3 = 2.666
-% Expected health remaining = 3.5 - 2.666 = .834
+%%
+% Expected troll starting health $.5*3 + .5*4 = 3.5$
+%% 
+% Expected fireball damage $1/3*2 + 2/3*3 = 2.666$
+%% 
+% Expected health remaining $= 3.5 - 2.666 = .834$
 
 % Don't feel like simming 
 hp = randi([3,4],M,1); % make 3 or 4 health troll
